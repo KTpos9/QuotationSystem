6b00b0ba595e6dd6113359b494ac9541adf0931d
kittiphop.z <kittiphop.z@ICNB1603096>
2023-06-20 16:53:46 +0700

    add more properties to viewmodel

--- QuotationSystem/Models/Quotation/QuotationViewModel.cs
+++ QuotationSystem/Models/Quotation/QuotationViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using QuotationSystem.Data.Models;
+using System;
+using System.Collections.Generic;
 
 namespace QuotationSystem.Models.Quotation
 {
@@ -6,5 +8,14 @@ namespace QuotationSystem.Models.Quotation
     {
         public TQuotationHeader QuotationHeader { get; set; }
         public string QuotationNo { get; set; }
+        public DateTime Date { get; set; }
+        public string Customer { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerContact { get; set; }
+        public string TaxId { get; set; }
+        public string SalesName { get; set; }
+        public int Vat { get; set; }
+        public string ActiveStatus { get; set; }
+        public List<MItem> ItemList { get; set; }
     }
 }
