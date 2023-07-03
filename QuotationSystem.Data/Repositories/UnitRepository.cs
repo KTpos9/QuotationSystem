a86580aa3750b4f208f2f4cba5f78e39dbb173e6
kittiphop.z <kittiphop.z@ICNB1603096>
2023-07-03 14:31:33 +0700

    add unit repository

---
+++ QuotationSystem.Data/Repositories/UnitRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using QuotationSystem.Data.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotationSystem.Data.Repositories
+{
+    public class UnitRepository : IUnitRepository
+    {
+        private readonly DbContextOptionBuilder option;
+
+        public UnitRepository(DbContextOptionBuilder option)
+        {
+            this.option = option;
+        }
+        public List<SelectListItem> GetAllUnitIds()
+        {
+            using (var db = new QuotationContext(option))
+            {
+                return db.MUnits.Select(d => new SelectListItem
+                {
+                    Text = d.UnitId,
+                    Value = d.UnitId
+                }).ToList();
+            }
+        }
+    }
+}
