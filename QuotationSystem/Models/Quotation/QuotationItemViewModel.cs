2c2fed099cac62a0efe30fba6d11ecf35984d553
kittiphop.z <kittiphop.z@ICNB1603096>
2023-07-14 16:31:43 +0700

    add view model for item for add quotation page

---
+++ QuotationSystem/Models/Quotation/QuotationItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotationSystem.Models.Quotation
+{
+    public class QuotationItemViewModel
+    {
+        public string itemCode { get; set; }
+        public string itemName { get; set; }
+        public string itemDesc { get; set; }
+        public double Qty { get; set; }
+        public string unitId { get; set; }
+        public string unitPrice { get; set; }
+        public double discount { get; set; }
+        public string total { get; set; }
+    }
+}
