d4c80ecef9565270d8d6657853fbf02d8650aa9c
kittiphop.z <kittiphop.z@ICNB1603096>
2023-07-19 18:15:32 +0700

    change vat property's type

--- QuotationSystem/Models/Quotation/QuotationViewModel.cs
+++ QuotationSystem/Models/Quotation/QuotationViewModel.cs
@@ -14,8 +14,8 @@ namespace QuotationSystem.Models.Quotation
         public string CustomerContact { get; set; }
         public string TaxId { get; set; }
         public string SalesName { get; set; }
-        public int Vat { get; set; }
+        public double Vat { get; set; }
         public string ActiveStatus { get; set; }
-        public List<MItem> ItemList { get; set; }
+        //public List<MItem> ItemList { get; set; }
     }
 }
