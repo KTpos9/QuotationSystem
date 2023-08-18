45e8379c5472c12c5e05d77ad3946d2ae9b2264c
kittiphop.z <kittiphop.z@ICNB1603096>
2023-08-18 11:10:17 +0700

    add test suite to the project

---
+++ QuotationSystem.Test/Pages/QuotationManagementPage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace QuotationSystem.Test.Pages
+{
+    public class QuotationManagementPage
+    {
+        private IPage _page; 
+        public QuotationManagementPage(IPage page) => _page = page;
+
+        public async Task Search(string searchTerm)
+        {
+
+        }
+    }
+}
