1448d3773c88c0b7a1899518201dabe3855f9b49
kittiphop.z <kittiphop.z@ICNB1603096>
2023-08-21 09:51:50 +0700

    add item management page test

--- QuotationSystem.Test/Pages/ItemManagementPage.cs
+++ QuotationSystem.Test/Pages/ItemManagementPage.cs
@@ -8,5 +8,16 @@ namespace QuotationSystem.Test.Pages
         private IPage _page;
 
         public ItemManagementPage(IPage page) => _page = page;
+
+        private ILocator textItemCode => _page.Locator("#textId");
+        private ILocator textItemName => _page.Locator("#textName");
+        private ILocator btnSearch => _page.Locator("#search-button");
+
+        public async Task Search(string itemCode = "", string itemName = "")
+        {
+            await textItemCode.FillAsync(itemCode);
+            await textItemName.FillAsync(itemName);
+            await btnSearch.ClickAsync();
+        }
     }
 }
